5fe20743a8e0bd765839c6c2ee1e3e67411d552a
WooYong333 <81574524+WooYong333@users.noreply.github.com>
2021-08-12 11:15:55 +0900

    Finished with App.js for now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import react from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import axios from "axios";
 
-function App() {
+class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
-  componentDidMount(); {
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 6000);
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    this.setState({movies:movies, isLoading: false})
+  };
+  componentDidMount() {
+    this.getMovies();
   }
-  render(); {
+  render() {
     const {isLoading} = this.state;
     return <div>{isLoading ? "Loading..." : "We are ready!"}</div>;
   }
